c90aac6a7ef24ce34a14be98d21da22b870ceb0e
student <student@CAB4-COMP10>
2025-10-03 12:14:59 +1000

    wwdq

--- MoOnStarDG/SportsMan.xaml.cs
+++ MoOnStarDG/SportsMan.xaml.cs
@@ -21,31 +21,66 @@ namespace MoOnStarDG
     /// <summary>
     /// Логика взаимодействия для SportsMan.xaml
     /// </summary>
-    public partial class SportsMan : Window
+    public partial class SportsMan : Window, INotifyPropertyChanged
     {
         MoOnStarDgContext db;
+        private List<Sportsman> sportsmen;
+        private List<Training> training;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void Signal([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
         public SportsMan()
         {
             InitializeComponent();
             db = new MoOnStarDgContext();
+            DataContext = this;
             LoadData();
+        }
+
+        public List<Sportsman> Sportsmen
+        {
+            get => sportsmen;
+            set
+            {
+                sportsmen = value;
+                Signal();
+            }
+        }
+        public List<Training> Training
+        {
+            get => training;
+            set
+            {
+                training = value;
+                Signal();
+            }
         }
 
         private void LoadData()
         {
             try
             {
-                var sportsmen = db.Sportsmen
+                //var sportsmen = db.Sportsmen
+                //    .Include(s => s.IdCategoryNavigation)
+                //    .Include(s => s.IdLevelNavigation)
+                //    .ToList();
+                //SportsmenList.ItemsSource = sportsmen;
+
+                //var trainings = db.Training
+                //    .Include(t => t.Type)
+                //    .ToList();
+                //SportsmenList.ItemsSource = trainings;
+                //Signal(nameof(SportsmenList));
+
+                Sportsmen = db.Sportsmen
                     .Include(s => s.IdCategoryNavigation)
                     .Include(s => s.IdLevelNavigation)
                     .ToList();
-                SportsmenList.ItemsSource = sportsmen;
-
-                var trainings = db.Training
-                    .Include(t => t.Type)
+                Training = db.Training
+                    .Include(n => n.Type)
                     .ToList();
-                SportsmenList.ItemsSource = trainings;
+
             }
             catch (Exception ex)
             {
@@ -53,7 +88,27 @@ namespace MoOnStarDG
                               MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
-
+        private void Grade5(object sender, RoutedEventArgs e)
+        {
+            if (TrainingsList.SelectedItem == null)
+            {
+                MessageBox.Show("выберете кому хотите поставить оценку");
+                return;
+            }
+            else if (TrainingsList.SelectedItem != null)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("ты еблан, ты как это сделал");
+            }
+        }
+        private void BattonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            Signal();
+            LoadData();
+        }
         private void BackToMainBtn_Click(object sender, RoutedEventArgs e)
         {
             MainWindow mainWindow = new MainWindow();
